d4d0651ab548b2ad850c8a47a9617ec0324412b1
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2022-04-24 17:43:12 +0200

    making testimonials  content and all css on it for desktop and mobile devices + swiperjs done with pagination

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ closeNavBtn.addEventListener('click', () => {
     closeNavBtn.style.display = 'none';
     openNavBtn.style.display = 'inline-block';
 })
+
+
+// TESTIMONIALS SECTION (swiperjs)
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 600px
+        600: {
+          slidesPerView: 2
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 3
+        }
+      }
+  });
